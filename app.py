f370bd7e9836bb46a5f2c72c05d0f6803f3c50eb
Harley Trung <harley@coderpush.com>
2024-09-03 14:07:49 +0700

    refactor to keep runpod code separate

--- app.py
+++ app.py
@@ -2,22 +2,34 @@ import chainlit as cl
 from openai import AsyncOpenAI
 import os
 
-# api_key = os.getenv("OPENAI_API_KEY")
-api_key = os.getenv("RUNPOD_API_KEY")
-runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
 
-# endpoint_url = "https://api.openai.com/v1"
-endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
+def get_openai_client():
+    api_key = os.getenv("OPENAI_API_KEY")
+    endpoint_url = "https://api.openai.com/v1"
+    client = AsyncOpenAI(api_key=api_key, base_url=endpoint_url)
+    model_kwargs = {"model": "chatgpt-4o-latest", "temperature": 0.5, "max_tokens": 500}
+    return client, model_kwargs
 
-client = AsyncOpenAI(api_key=api_key, base_url=endpoint_url)
 
-# https://platform.openai.com/docs/models/gpt-4o
-# model_kwargs = {"model": "chatgpt-4o-latest", "temperature": 0.5, "max_tokens": 500}
-model_kwargs = {
-    "model": "mistralai/Mistral-7B-Instruct-v0.3",
-    "temperature": 0.5,
-    "max_tokens": 500,
-}
+def get_runpod_client():
+    api_key = os.getenv("RUNPOD_API_KEY")
+    runpod_serverless_id = os.getenv("RUNPOD_SERVERLESS_ID")
+    endpoint_url = f"https://api.runpod.ai/v2/{runpod_serverless_id}/openai/v1"
+    client = AsyncOpenAI(api_key=api_key, base_url=endpoint_url)
+    model_kwargs = {
+        "model": "mistralai/Mistral-7B-Instruct-v0.3",
+        "temperature": 0.5,
+        "max_tokens": 500,
+    }
+    return client, model_kwargs
+
+
+use_openai = True  # Set this to False to use RunPod endpoint
+
+if use_openai:
+    client, model_kwargs = get_openai_client()
+else:
+    client, model_kwargs = get_runpod_client()
 
 
 @cl.on_message
